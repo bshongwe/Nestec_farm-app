c8f4c812be54b710d5c4a4b04dc3d3096a9e4c30
bshongwe <dru.da.beat.hustle@gmail.com>
2024-03-30 18:28:41 +0200

    <MongoDB + Redis> >> server

---
+++ node-redis-mongodb-backend/src/server/middlewares/RedisMiddleware.js
@@ -0,0 +1,37 @@
+export default class RedisMiddleware {
+  constructor(app, redis) {
+    this.app = app;
+    this.redisClient = redis;
+  }
+
+  cacheCheck(req, res, next, self) {
+    const key = self.generateCacheKey(req.path, req.query.age, req.query.page);
+    self.redisClient.get(key, (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        // Check cached data is exist.
+        if (data != null) {
+          // Send cached data.
+          console.log(`Response is sent from cache with key: ${key}`);
+          res.status(200).send(JSON.parse(data));
+        } else {
+          // If there is no cached data then let leave the caching to the
+          // final controller function.
+          console.log(`Response is not sent from cache with key: ${key}`);
+          next();
+        }
+      }
+    });
+  }
+
+  /**
+   * This function will create a key for making the request uniq.
+   * 
+   * @param {*Url path} path 
+   * @param {*Url page} page 
+   */
+  generateCacheKey(path, page) {
+    return `${path}_${page}`;
+  }
+}
